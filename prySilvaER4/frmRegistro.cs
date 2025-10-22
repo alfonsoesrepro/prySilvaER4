b92bf52344a7eb08627a3a032e23b83ec3a3c4bc
alfonsoesrepro <alfonsoesrepro@gmail.com>
2025-10-22 12:35:34 -0300

    Pendiente de terminar.

--- prySilvaER4/frmRegistro.cs
+++ prySilvaER4/frmRegistro.cs
@@ -19,6 +19,8 @@ namespace prySilvaER4
             dgvRegistro.Rows.Add("Javier");
             dgvRegistro.Rows.Add("Gonzalo");
             dgvRegistro.Rows.Add("Alberto");
+
+
         }
     }
 }
